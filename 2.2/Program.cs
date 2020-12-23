aedecc63b527ea834e91c3b85a52feb560cb0076
KonungGudleifr <72839083+KonungGudleifr@users.noreply.github.com>
2020-12-23 19:50:36 +0200

    Add files via upload

---
+++ 2.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChernobylApp2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите число a: ");
+                ulong a = ulong.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число b: ");
+                ulong b = ulong.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число c: ");
+                ulong c = ulong.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число d: ");
+                ulong d = ulong.Parse(Console.ReadLine());
+                if ((a < c) && (b < d))
+                    Console.WriteLine("Можно");
+                else if ((a < d) && (b < c)) ;
+                else Console.WriteLine("Не можно");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
